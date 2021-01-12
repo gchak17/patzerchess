3d81b16ee3860e198a88fce00137887ffa916e5e
gchak17 <gchak17@freeuni.edu.ge>
2021-01-13 02:30:25 +0400

    added websockets on client and server

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+const ws = require("express-ws")(app);
 
-app.use(express.static('public'))
+let rooms = {};
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.post("/room", (req, res) => {
+  const roomId = uuidv4();
+  res.redirect(`/room/${roomId}`);
+});
+
+app.get("/room/:id", (req, res) => {
+  res.sendFile(path.join(__dirname + "/room.html"));
+});
+
+app.ws("/room/:id", (ws, req) => {
+  let id = req.params.id;
+  ws.on("message", (data) => {
+    let message = JSON.parse(data);
+    switch (message.type) {
+      case "connection":
+        if (!rooms[id]) {
+          rooms[id] = {};
+          rooms[id]["player1"] = ws;
+        } else {
+          rooms[id]["player2"] = ws;
+        }
+        break;
+      default:
+        break;
+    }
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`)
-})
+  console.log(`Listening at http://localhost:${port}`);
+});

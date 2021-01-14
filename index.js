d1a7288a6064ba4d58a02400866d3764c2954ab8
gchak17 <gchak17@freeuni.edu.ge>
2021-01-14 21:38:24 +0400

    starting code of game chat

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const ws = require("express-ws")(app);
 
 let rooms = {};
 
-app.get("/", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
@@ -32,6 +32,10 @@ app.ws("/room/:id", (ws, req) => {
         } else {
           rooms[id]["player2"] = ws;
         }
+        break;
+      case "message":
+        rooms[id]["player1"].send(JSON.stringify(message));
+        rooms[id]["player2"].send(JSON.stringify(message));
         break;
       default:
         break;
